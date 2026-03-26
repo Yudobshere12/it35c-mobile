0fdaa36e3a2e3abb03ad6154f1ee49a274c84b54
Sopiyah02 <sopialorrainesolana@gmail.com>
2026-03-26 15:12:10 +0800

    Add a theming color

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,32 +1,52 @@
 import React from 'react';
-import { IonAlert, IonButton } from '@ionic/react';
+import { IonBadge, IonTabBar, IonTabButton, IonIcon, IonLabel, IonItem, IonList } from '@ionic/react';
+import { heart, calendar, musicalNote } from 'ionicons/icons';
 
 function Example() {
   return (
     <>
-      <IonButton id="present-alert">Click Me</IonButton>
-      <IonAlert
-        trigger="present-alert"
-         header="Select your favorite color"
-        buttons={['OK']}
-        inputs={[
-          {
-            label: 'Red',
-            type: 'radio',
-            value: 'red',
-          },
-          {
-            label: 'Blue',
-            type: 'radio',
-            value: 'blue',
-          },
-          {
-            label: 'Green',
-            type: 'radio',
-            value: 'green',
-          },
-        ]}
-      ></IonAlert>
+      <IonTabBar color="light">
+        <IonTabButton tab="tab1">
+          <IonIcon icon={heart} />
+          <IonLabel>Favorites</IonLabel>
+          <IonBadge color="danger"></IonBadge>
+        </IonTabButton>
+        <IonTabButton tab="tab2">
+          <IonIcon icon={musicalNote} />
+          <IonLabel>Music</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="tab3">
+          <IonIcon icon={calendar} />
+          <IonLabel>Calendar</IonLabel>
+          <IonBadge color="danger">47</IonBadge>
+        </IonTabButton>
+      </IonTabBar>
+       <IonList>
+      <IonItem>
+        <IonLabel>Followers</IonLabel>
+        <IonBadge color="primary">22k</IonBadge>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Likes</IonLabel>
+        <IonBadge color="secondary">118k</IonBadge>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Stars</IonLabel>
+        <IonBadge color="tertiary">34k</IonBadge>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Completed</IonLabel>
+        <IonBadge color="success">80</IonBadge>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Warnings</IonLabel>
+        <IonBadge color="warning">70</IonBadge>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Notifications</IonLabel>
+        <IonBadge color="danger">1000</IonBadge>
+      </IonItem>
+    </IonList>
     </>
   );
 }
