955475bcfdadaaca3ac94f525fa0c9f242cdca04
yudobshere12 <yudobsheremijares@gmail.com>
2026-04-20 15:52:18 +0800

    added a feed and search bar

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,19 +1,78 @@
-import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonLabel, IonItem, IonList, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonGrid, IonRow, IonCol, IonIcon} from '@ionic/react';
+import { chatboxEllipses, chatboxEllipsesOutline } from 'ionicons/icons';
 
 const Feed: React.FC = () => {
+  const games =[
+  {name:"Pokémon Yellow"},
+  {name:"Mega Man X"},
+  {name:"The Legend of Zelda"},
+  {name:"Pac-Man"},
+  {name:"Super Mario World"}
+  
+]
   return (
     <IonPage>
         
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>login</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        <h1>Feed</h1>
-      </IonContent>
-    </IonPage>
-  );
+         <IonToolbar>
+           <IonTitle>login</IonTitle>
+              </IonToolbar>
+                  </IonHeader>
+                       <IonContent className="ion-padding">
+
+                         <IonList>
+                          {games.map((item,index)=>(
+                            <IonItem key={index}>
+                              <IonLabel>{item.name}</IonLabel>
+                            </IonItem>
+                          ))}
+                          
+               </IonList>
+
+               <IonCard>
+                   <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+                      <IonCardHeader>
+                        <IonCardTitle>YUDOBSHERE</IonCardTitle>
+                          <IonCardSubtitle>YAPHETS</IonCardSubtitle>
+                             </IonCardHeader>
+
+                                  <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+                                    </IonCard>
+
+                               <IonGrid>
+                                 <IonRow>
+                                   <IonCol>
+                                    <IonButton fill="clear" expand="full">
+                                      <IonIcon icon={chatboxEllipsesOutline} ></IonIcon>
+                                        <IonLabel style={{marginLeft:'5px'}}>Comment</IonLabel>
+                                    </IonButton>
+                                   </IonCol>
+
+                                    <IonButton fill="clear" expand="full">
+                                      <IonIcon icon={chatboxEllipsesOutline} ></IonIcon>
+                                        <IonLabel style={{marginLeft:'5px'}}>LIKE</IonLabel>
+                                    </IonButton>
+
+                                      <IonButton fill="clear" expand="full">
+                                      <IonIcon icon={chatboxEllipsesOutline} ></IonIcon>
+                                        <IonLabel style={{marginLeft:'5px'}}>share</IonLabel>
+                                    </IonButton>
+
+                                    
+
+                                   
+                                     <IonCol></IonCol>
+                                       <IonCol></IonCol>
+                                           </IonRow>
+                                                </IonGrid>
+
+
+
+
+
+               </IonContent>
+         </IonPage>
+   );
 };
 
 export default Feed;
--- src/pages/home-tabs/Search.tsx
+++ src/pages/home-tabs/Search.tsx
@@ -1,19 +1,69 @@
-import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonLabel, IonItem, IonList, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonGrid, IonRow, IonCol, IonIcon, IonSearchbar} from '@ionic/react';
+import { chatboxEllipses, chatboxEllipsesOutline, filter } from 'ionicons/icons';
+import { useEffect, useState } from 'react';
 
-const Search: React.FC = () => {
+const Feed: React.FC = () => {
+  const games =[
+  {name:"Pokémon Yellow"},
+  {name:"Mega Man X"},
+  {name:"The Legend of Zelda"},
+  {name:"Pac-Man"},
+  {name:"Super Mario World"}
+  
+]
+
+    const [searchText, setSearchText] = useState("");
+    const [filtered, setFilteredGames] = useState(games);
+
+
+    useEffect(()=>{
+      const debounce = setTimeout(()=>{
+        const filteredGames = games.filter((game)=> 
+          game.name.toLowerCase().includes(searchText.toLowerCase())
+        );
+        setFilteredGames(filteredGames);
+      }, 400);
+      
+      return () => clearTimeout(debounce);
+    }, [searchText]);
+
+    
   return (
     <IonPage>
         
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>login</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        <h1>Search</h1>
-      </IonContent>
-    </IonPage>
-  );
+         <IonToolbar>
+           <IonTitle>SEARCH</IonTitle>
+              </IonToolbar>
+                  </IonHeader>
+                       <IonContent className="ion-padding">
+
+                         
+                            <IonSearchbar
+                              placeholder="Search games..."
+                              value={searchText}
+                              debounce ={0}
+                              onIonInput={(e) => setSearchText(e.detail.value!)}
+                            />
+                            
+
+
+
+                         <IonList>
+                          {filtered.map((item,index)=>(
+                            <IonItem key={index}>
+                              <IonLabel>{item.name}</IonLabel>
+                            </IonItem>
+                          ))}
+                          
+               </IonList>
+
+              
+
+
+               </IonContent>
+         </IonPage>
+   );
 };
 
-export default Search;
+export default Feed;
